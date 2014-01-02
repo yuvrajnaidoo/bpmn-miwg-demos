5c43acbfe04f6ba3aeeb8577f8426f15d1980a26
Falko Menge <falko.menge@camunda.com>
2014-01-02 19:31:31 +0100

    Remove examples not used by BPMN MIWG

--- bank-account-opening-mule/src/main/java/com/camunda/fox/rest/FoxRestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.camunda.fox.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class FoxRestApplication extends Application {
-
-}
--- twitter-drools/src/main/java/org/camunda/bpm/example/twitter/TwitterServletProcessApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.example.twitter;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication
-public class TwitterServletProcessApplication extends ServletProcessApplication {
-
-}
